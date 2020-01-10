44e33c629d39aee27ad8405f664f29e45f1c1aa5
Michael Milke <desmilke@gmail.com>
2020-01-10 12:45:44 +0100

    React random lists with 2 sexes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
 const Person = ({img, name, job, children}) => {
+    const url = `https://randomuser.me/api/portraits/thumb/women/${img}.jpg`
+    return (
+        <article className="person">
+            <img src= {url}
+             alt="person"
+            /> 
+            <h4>{name}</h4>
+            <h4>{job}</h4>
+            {children}
+        </article>
+    )
+}
+
+const Person1 = ({img, name, job, children}) => {
     const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`
     return (
         <article className="person">
@@ -16,13 +30,19 @@ const Person = ({img, name, job, children}) => {
 }
 
 
-
 const PersonList = () => {
     return (
     <section className="person-list">
-        <Person img="13" name="John" job="developer" />
-        <Person img="69" name="Harry" job="architect" />
-        <Person img="10" name="Mobby" job="designer">
+        <Person1 img="13" name="John" job="developer" />
+        <Person1 img="69" name="Harry" job="architect" />
+        <Person1 img="10" name="Mobby" job="designer">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, inventore.
+                </p>
+        </Person1>
+        <Person img="13" name="Lucy" job="developer" />
+        <Person img="69" name="Mandy" job="architect" />
+        <Person img="10" name="Juliette" job="designer">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, inventore.
                 </p>
